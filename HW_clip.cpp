2077be3186f20f6df61f794d2daa05988b1fe1d3
xchen008 <38867173+xchen008@users.noreply.github.com>
2019-09-23 12:28:36 -0400

    HW1-Clip

---
+++ HW_clip.cpp
@@ -0,0 +1,39 @@
+#include "IP.h"
+using namespace IP;
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// HW_clip:
+//
+// Clip intensities of image I1 to [t1,t2] range. Output is in I2.
+// If    input<t1: output = t1;
+// If t1<input<t2: output = input;
+// If      val>t2: output = t2;
+//
+void
+HW_clip(ImagePtr I1, int t1, int t2, ImagePtr I2)
+{
+    // copy image header (width, height) of input image I1 to output image I2
+    IP_copyImageHeader(I1,I2);
+    
+    // init vars for width, height, and total number of pixels
+    int w = I1 -> width();
+    int h = I1 -> height();
+    int total = w * h;
+    
+    // init lookup table
+    int i, lut[MXGRAY];
+	for(i = 0; i < t1; i++) lut[i] = t1;
+	for(; i < t2; i++) lut[i] = i;
+	for(; i < MXGRAY; i++) lut[i] = t2;
+    
+    // declarations for image channel pointers and datatype
+    ChannelPtr<uchar> p1, p2;
+    int type;
+
+    // visit all image channels and evaluate output image
+    for(int ch=0; IP_getChannel(I1, ch, p1, type); ch++) {    // get input  pointer for channel ch
+        IP_getChannel(I2, ch, p2, type);
+            // get output pointer for channel ch
+        for(i=0; i<total; i++) *p2++ = lut[*p1++];    // use lut[] to eval output
+    }
+}
